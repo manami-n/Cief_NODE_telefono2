b40c8d7aba80b53044a84654bd27f55e56b07752
Manami Naito <manamy.naito@gmail.com>
2024-05-27 13:41:12 +0200

    Second Commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 import express from 'express';
 //const express = require('express');
 const app = express();
-// const bodyParser = require('body-parser'); // it's npm module.
 
 // Importado las rutas
 //const rutas = require('./rutas')
